df5500a6ba0709faf2f5f12d7a4a1a23cde60652
cjfow <cfowler1219@gmail.com>
2025-06-05 22:11:13 -0400

    Refactored battle.cs

--- obj/Release/net9.0/turn-based-game.AssemblyInfo.cs
+++ obj/Release/net9.0/turn-based-game.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("turn-based-game")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6b2d63357e15cd3e873f8486c38535d49ff169e4")]
 [assembly: System.Reflection.AssemblyProductAttribute("turn-based-game")]
