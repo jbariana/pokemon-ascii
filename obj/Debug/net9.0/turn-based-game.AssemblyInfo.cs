7f7c2fcb0c52d66fbab59a987336184a6f7ce6e8
cjfow <cfowler1219@gmail.com>
2025-06-05 22:01:27 -0400

    c

--- obj/Debug/net9.0/turn-based-game.AssemblyInfo.cs
+++ obj/Debug/net9.0/turn-based-game.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("turn-based-game")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e0fe3d2b4aa0424ee64ca8fc54c50b16687b99b6")]
 [assembly: System.Reflection.AssemblyProductAttribute("turn-based-game")]
 [assembly: System.Reflection.AssemblyTitleAttribute("turn-based-game")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
